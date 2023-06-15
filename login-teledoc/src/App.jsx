dfc14cff881baab8e3110ce3e49e0669f364d138
Johan Febriansyah <johanfebriansyah@students.unnes.ac.id>
2023-06-15 10:54:19 +0700

    1

--- login-teledoc/src/App.jsx
+++ login-teledoc/src/App.jsx
@@ -1,14 +1,18 @@
 import Register from "./Register";
 import Login from "./login";
-import { Switch } from 'react-router-dom';
+// import { Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <main className="App">
       <Login />
-      <Switch>
-        <Route path="/Register" component={Register} />
-      </Switch>
+      <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
     </main>
   );
 }
