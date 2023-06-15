1cf05f9bc6687d252a6db93a047c35597a8d377d
Johan Febriansyah <johanfebriansyah@students.unnes.ac.id>
2023-06-15 21:38:37 +0700

    6

--- login-teledoc/src/Api/axios.js
+++ login-teledoc/src/Api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:5173/'
+    baseURL: 'https://mockapi.io/projects/6452fa93bce0b0a0f7532c94'
 });
--- login-teledoc/src/App.jsx
+++ login-teledoc/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route exact path="/login" component={Login} />
+        <Route path="/login" component={Login} />
       </Routes>
     </BrowserRouter>
     </main>
